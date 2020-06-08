6af5273416c15277a07f9a2f9cb1c5e08fe77a84
skindra <skindrapurnama@gmail.com>
2020-06-08 21:49:11 +0700

    sampai tb_kelas dan modul kelas

---
+++ application/controllers/Kelas.php
@@ -0,0 +1,152 @@
+<?php
+
+Class Kelas extends OperatorController {
+
+    function __construct() {
+        parent::__construct();
+        // if (empty($this->session->userdata('username'))) {
+        //     redirect('auth');
+        // }
+        //chekAksesModule();
+        $this->load->library('ssp');
+        $this->menu = "master";
+        $this->sub_menu = "kelas";
+        // $this->load->model('Model_gelombang');
+    }
+
+    
+    function data() {
+        // nama tabel
+        $table = 'tb_kelas';
+        // nama PK
+        $primaryKey = 'id_kelas';
+        // list field
+        $columns = array(
+            array('db' => 'id_kelas', 'dt' => 'id_kelas'),
+            array('db' => 'nama_kelas', 'dt' => 'nama_kelas'),
+            array('db' => 'status_kelas', 'dt' => 'status_kelas'),
+            array(
+                'db' => 'id_kelas',
+                'dt' => 'aksi',
+                'formatter' => function( $d) {
+                    //return "<a href='edit.php?id=$d'>EDIT</a>";
+                    return anchor('gelombang/edit/'.$d,'<i class="fa fa-edit"></i>','class="btn btn-xs btn-teal tooltips" data-placement="top" data-original-title="Edit"').' 
+                        '.anchor('gelombang/delete/'.$d,'<i class="fa fa-trash"></i>','class="btn btn-xs btn-danger tooltips" data-placement="top" data-original-title="Delete" onclick="return confirm(\'Are you sure delete?\')"');
+                }
+            )
+        );
+
+        $sql_details = array(
+            'user' => $this->db->username,
+            'pass' => $this->db->password,
+            'db' => $this->db->database,
+            'host' => $this->db->hostname
+        );
+
+        echo json_encode(
+                SSP::simple($_GET, $sql_details, $table, $primaryKey, $columns)
+        );
+    }
+
+    function index() {
+        $data['heading']    = $this->template->link('Kelas ');
+        $data['menu'] = $this->menu;
+        $data['sub_menu'] = $this->sub_menu;
+        $this->template->load('template', 'kelas/list' ,$data);
+    }
+
+    
+    function add() {
+        // if (isset($_POST['submit'])) {
+        //     $uplodFoto = $this->upload_foto_user();
+        //     $this->Model_users->save($uplodFoto);
+        //     redirect('users');
+        // } else {
+            // $data['Judul'] = 'Tambah Data';
+            // $this->template->load('template', 'gelombang/add',$data);
+        // }
+        
+        if (!$_POST) {
+            $data['input'] = (object) $this->Model_gelombang->getDefaultValues();
+        } else {
+            $data['input'] = (object) $this->input->post(null, true);
+        }
+
+        if (!$this->Model_gelombang->validate()) {
+            // $halaman     = $this->halaman;
+            $data['mainView']   = 'gelombang/add';
+            $data['heading']    = $this->template->link('Gelombang > Tambah');
+            $data['formAction'] = "gelombang/add";
+            $data['buttonText'] = 'Tambah';
+            $data['menu']       = $this->menu;
+            $data['sub_menu']   = $this->sub_menu;
+            $this->template->load('template', $data['mainView'],$data);
+            // $this->load->view('template', compact('halaman', 'main_view', 'form_action', 'input'));
+            return;
+        }
+
+        if ($this->Model_gelombang->insert($data['input'])) {
+            $this->session->set_flashdata('success', 'Data gelombang berhasil disimpan.');
+        } else {
+            $this->session->set_flashdata('error', 'Data kelas gagal disimpan.');
+        }
+
+        redirect('gelobang');
+    }
+
+    
+    public function edit($id = null)
+    {
+        $gelombang = $this->Model_gelombang->find('id_kelas',$id);
+        if (!$gelombang) {
+            flashMessage('error', 'Data tidak ditemukan!');
+            redirect('gelombang', 'refresh');
+        }
+
+        $data['input'] = (object) $this->input->post(null, true);
+        if (! $_POST) {
+            $data['input'] = (object) $gelombang;
+        }
+
+        $validate = $this->Model_gelombang->validate();
+        if (! $validate) {
+            $data['mainView']   = 'gelombang/add';
+            $data['heading']    = $this->template->link('Gelombang > Edit ');
+            $data['formAction'] = "gelombang/edit/$id";
+            $data['buttonText'] = 'Update';
+            $data['menu'] = $this->menu;
+            $data['sub_menu'] = $this->sub_menu;
+            $this->template->load('template', $data['mainView'] ,$data);
+            return;
+        }
+
+        $update = $this->Model_gelombang->update($id, $data['input'],'id_kelas');
+        if (! $update) {
+            flashMessage('error', 'Data gagal diupdate!');
+        } else {
+            flashMessage('success', 'Data berhasil diupdate.');
+        }
+
+        redirect('gelombang', 'refresh');
+    }
+
+    public function delete($id)
+    {
+        $gelombang = $this->Model_gelombang->find('id_kelas',$id);
+        if (!$gelombang) {
+            flashMessage('error', 'Data tidak ditemukan!');
+            redirect('gelombang', 'refresh');
+        }
+
+        $hapus = $this->Model_gelombang->where('id_kelas',$id)->delete();
+
+        if (!$hapus) {
+            flashMessage('error', 'Data gagal dihapus!');
+        } else {
+            flashMessage('success', 'Data berhasil dihapus.');
+        }
+        
+        redirect('gelombang', 'refresh');
+    }
+
+}
---
+++ application/models/Model_kelas.php
@@ -0,0 +1,62 @@
+<?php
+
+class Model_kelas extends MY_Model {
+
+    public $table ="tb_kelas";
+    
+    // function save() {
+    //     $data = array(
+    //         'nuptk'      => $this->input->post('nuptk', TRUE),
+    //         'nama_guru'  => $this->input->post('nama_guru', TRUE),
+    //         'gender'     => $this->input->post('gender', TRUE),
+    //         'username'   => $this->input->post('username', TRUE),
+    //         'password'   => md5($this->input->post('password', TRUE))
+    //     );
+    //     $this->db->insert($this->table,$data);
+    // }
+    
+    // function update() {
+    //     $data = array(
+    //         'nuptk'      => $this->input->post('nuptk', TRUE),
+    //         'nama_guru'  => $this->input->post('nama_guru', TRUE),
+    //         'gender'     => $this->input->post('gender', TRUE)
+    //     );
+    //     $id_guru   = $this->input->post('id_guru');
+    //     $this->db->where('id_guru',$id_guru);
+    //     $this->db->update($this->table,$data);
+    // }
+    
+    // function chekLogin($username,$password){
+    //     $this->db->where('username',$username);
+    //     $this->db->where('password',  md5($password));
+    //     $user = $this->db->get('tbl_guru')->row_array();
+    //     return $user;
+    // }
+
+    public function getValidationRules()
+    {
+        $validationRules = [
+            [
+                'field' => 'nama_kelas',
+                'label' => 'Nama Gelombang',
+                'rules' => 'trim|required|min_length[1]|max_length[30]'
+            ],
+            [
+                'field' => 'status_kelas',
+                'label' => 'Nama Gelombang',
+                'rules' => 'trim|required|min_length[1]|max_length[9]'
+            ],
+        ];
+
+        return $validationRules;
+    }
+
+    public function getDefaultValues()
+    {
+        return [
+            'nama_kelas'    => '',
+            'status_kelas'    => '',
+        ];
+    }
+
+}
